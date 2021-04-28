c90314b1c94b12585d06bd098d0cb1984d4a1f1e
unknown <mfsu2@yahoo.com>
2021-04-28 13:15:07 -0400

    uploading files to github

---
+++ animals.js
@@ -0,0 +1,54 @@
+const junieta = document.querySelector('.jumboImgJu')
+const billy = document.querySelector('.jumboImgBi')
+const profileA = document.querySelector('.cardA')
+const profileB = document.querySelector('.cardB')
+const buttonBlue = document.querySelector('.buttonA')
+const buttonPink = document.querySelector('.buttonB')
+const close = document.querySelector('.closeA')
+const close2 = document.querySelector('.closeB')
+
+
+function load(){
+    junieta.classList.add('activeJunieta')
+    billy.classList.add('activeBilly')
+}
+
+
+buttonBlue.addEventListener('click', () =>{
+    profileA.classList.add('active')
+
+})
+buttonPink.addEventListener('click', () =>{
+    profileB.classList.add('active')
+
+})
+
+close.addEventListener('click', () =>{
+    profileA.classList.remove('active')
+})
+close2.addEventListener('click', () =>{
+    profileB.classList.remove('active')
+})
+
+window.smoothScroll = function(target) {
+    var scrollContainer = target;
+    do { //find scroll container
+        scrollContainer = scrollContainer.parentNode;
+        if (!scrollContainer) return;
+        scrollContainer.scrollTop += 1;
+    } while (scrollContainer.scrollTop == 0);
+
+    var targetY = 0;
+    do { //find the top of target relatively to the container
+        if (target == scrollContainer) break;
+        targetY += target.offsetTop;
+    } while (target = target.offsetParent);
+
+    scroll = function(c, a, b, i) {
+        i++; if (i > 30) return;
+        c.scrollTop = a + (b - a) / 23 * i;
+        setTimeout(function(){ scroll(c, a, b, i); }, 22);
+    }
+    // start scrolling
+    scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
+}
